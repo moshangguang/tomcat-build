948f70c3b544be0717ec72dacda498073839cd9e
林峰 <3208945136@qq.com>
2021-07-16 21:45:40 +0800

    构建tomcat

--- src/com/ch01/Response.java
+++ src/com/ch01/Response.java
@@ -5,58 +5,70 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-/*
-  HTTP Response = Status-Line
-    *(( general-header | response-header | entity-header ) CRLF)
-    CRLF
-    [ message-body ]
-    Status-Line = HTTP-Version SP Status-Code SP Reason-Phrase CRLF
-*/
-
 public class Response {
+    private static final int BUFFER_SIZE = 1024;
+    private Request request;
+    private OutputStream output;
 
-  private static final int BUFFER_SIZE = 1024;
-  Request request;
-  OutputStream output;
+    public void setRequest(Request request) {
+        this.request = request;
+    }
 
-  public Response(OutputStream output) {
-    this.output = output;
-  }
+    public Response(OutputStream output) {
+        this.output = output;
+    }
 
-  public void setRequest(Request request) {
-    this.request = request;
-  }
-
-  public void sendStaticResource() throws IOException {
-    byte[] bytes = new byte[BUFFER_SIZE];
-    FileInputStream fis = null;
-    try {
-      File file = new File(HttpServer.WEB_ROOT, request.getUri());
-      if (file.exists()) {
-        fis = new FileInputStream(file);
-        int ch = fis.read(bytes, 0, BUFFER_SIZE);
-        while (ch!=-1) {
-          output.write(bytes, 0, ch);
-          ch = fis.read(bytes, 0, BUFFER_SIZE);
+    public String parseContentType() {
+        if (request.getUri() == null) {
+            return null;
         }
-      }
-      else {
-        // file not found
-        String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
-          "Content-Type: text/html\r\n" +
-          "Content-Length: 23\r\n" +
-          "\r\n" +
-          "<h1>File Not Found</h1>";
-        output.write(errorMessage.getBytes());
-      }
+        if (request.getUri().endsWith(".html")) {
+            return "text/html";
+        }
+        if (request.getUri().endsWith(".png")) {
+            return "image/png";
+        }
+        return null;
     }
-    catch (Exception e) {
-      // thrown if cannot instantiate a File object
-      System.out.println(e.toString() );
+
+    public void sendStaticResource() throws IOException {
+        byte[] bytes = new byte[BUFFER_SIZE];
+        FileInputStream fis = null;
+        try {
+            File file = new File(HttpServer.WEB_ROOT, request.getUri());
+            if (file.exists()) {
+                StringBuffer content = new StringBuffer();
+                fis = new FileInputStream(file);
+                int ch = fis.read(bytes, 0, BUFFER_SIZE);
+                while (ch != -1) {
+                    for (int i = 0; i < ch; i++) {
+                        content.append((char) bytes[i]);
+                    }
+                    ch = fis.read(bytes, 0, BUFFER_SIZE);
+                }
+                String msg = "HTTP/1.1 200 OK\r\n";
+                if (request.getUri().endsWith("png")) {
+                    msg += "Accept-Ranges: bytes\r\n";
+                }
+                msg += "Content-Type: " + parseContentType() + "\r\n" +
+                        "Content-length: " + content.length() +
+                        "\r\n\r\n" + content;
+                output.write(msg.getBytes());
+            } else {
+                // file not found
+                String notFoundMsg = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" +
+                        "\r\n" +
+                        "<h1>File Not Found</h1>";
+                output.write(notFoundMsg.getBytes());
+            }
+            output.flush();
+        } finally {
+            if (fis != null) {
+                fis.close();
+            }
+        }
+
     }
-    finally {
-      if (fis!=null)
-        fis.close();
-    }
-  }
 }
